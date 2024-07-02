0908fe7de7ac790f3318a321addf49e82686d824
SUSAN ARYAL <77282787+arsusan@users.noreply.github.com>
2024-07-03 00:43:06 +0545

    fix range

--- loops/divisible_by_11.py
+++ loops/divisible_by_11.py
@@ -1,5 +1,5 @@
 # Write a program to display numbers which is divisible by only 11 from 100 to 300.
 print("Numbers divisible by 11 from 100 to 300.")
-for number in range(100,300):
+for number in range(100,301):
     if (number % 11) == 0:
         print(number)

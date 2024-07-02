f68b8dc191b058c972173d484bcaf1de9d159ae7
SUSAN ARYAL <77282787+arsusan@users.noreply.github.com>
2024-07-03 00:40:53 +0545

    add loops pratice program

--- loops/average_number.py
+++ loops/average_number.py
@@ -1,1 +1,12 @@
 # Accept 10 numbers from user and display average of it.
+numbers = []
+total = 0
+print("Enter 10 numbers to find averge.")
+for i in range (4):
+    num = int(input(f"Enter number {i+1}: "))  
+    numbers.append(num) 
+    total = sum(numbers)
+
+print(total)
+average = total / len(numbers)
+print(average)
